51720eb1a1f6d713670b06cca0d85cf5c9a5a9fe
Mu <mu@member.fsf.org>
2020-09-26 16:07:14 +0200

    eslint fucking 2 char indentation

--- src/services/label/label.service.js
+++ src/services/label/label.service.js
@@ -3,15 +3,15 @@ const { Label } = require('./label.class');
 const hooks = require('./label.hooks');
 
 module.exports = function (app) {
-  const options = {
-    paginate: app.get('paginate')
-  };
+    const options = {
+        paginate: app.get('paginate')
+    };
 
-  // Initialize our service with any options it requires
-  app.use('/label', new Label(options, app));
+    // Initialize our service with any options it requires
+    app.use('/label', new Label(options, app));
 
-  // Get our initialized service so that we can register hooks
-  const service = app.service('label');
+    // Get our initialized service so that we can register hooks
+    const service = app.service('label');
 
-  service.hooks(hooks);
+    service.hooks(hooks);
 };
